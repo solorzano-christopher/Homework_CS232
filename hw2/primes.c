ba71a5309c42b3f1b34cbb1540c428a91a99fa3b
Christopher Solorzano <christopher.o.solorzano@outlook.com>
2022-01-25 10:06:23 -0500

    Fixed output so tests can pass.

--- hw2/primes.c
+++ hw2/primes.c
@@ -20,25 +20,27 @@ int main(int argc, const char** argv){
     lowerBound = atoi(argv[1]);
     upperBound = atoi(argv[2]);
   
-    if(lowerBound < 1 || upperBound < 1)
+    if(lowerBound < 1 || upperBound < 1) //if they are lower than 1 the inputs are invalid. Either or.
     {
         fprintf(stderr, "ERROR: both the lowerBound (%d) and the upperBound (%d)"
 	          " must be positive.\n", lowerBound, upperBound);
         return -2;
     }
-        int increment; // a interger to increment to check the % ==0.
+        int increment; // a interger to increment to check if % == 0.
         for(lowerBound; lowerBound <= upperBound; lowerBound++)//must increment lowerBounds until it equals upperBound.
+                                                               //This will check all numbers from the given input.
         {
-            for(increment = 2; increment <= lowerBound; increment ++)//increasing increment intil value ==0
+            for(increment = 2; increment <= lowerBound; increment ++) //increasing increment intil value == 0
             {
-                if(lowerBound % increment == 0)
+                if(lowerBound % increment == 0) //If the remainder is 0 after divison. 
                 {
-                    break; //will break indicating that it has found a value that ==0
+                    break; //will break indicating that it has found a value that % == 0
                 }
             }
             if(lowerBound == increment)//if the values are the same it must be a prime number.
+                                       //Since % == 0 can only happen if they are the same in this point in the program. 
             {
-                    return lowerBound;//returns the prime number. 
+                printf("%d\n", lowerBound); //returning the prime number to screen or any tests that use text.
             }
         }
 
