14fc8a803aaf0c18477b627e7c5c2a1a0903be45
Christopher Solorzano <christopher.o.solorzano@outlook.com>
2022-03-27 16:23:48 -0400

    freeTrieMemory

--- project2/indexPage.c
+++ project2/indexPage.c
@@ -10,26 +10,52 @@
 
 /* TODO: structure definitions */
 
+typedef struct triNode
+{
+  int isLeaf; // a leaf checker, contains no children if  == 0.
+  int count; //keeps track of word occurance. 
+  struct trieNode *children[26]; // up to 26. one for each letter.
+}node;
 
-/* NOTE: int return values can be used to indicate errors (typically non-zero)
-   or success (typically zero return value) */
+node* newNode()
+{
+  node* head = (node*)malloc(sizeof(node));
+  head->isLeaf = 0;
+  head->count = 0;
+  for(int x = 0; x < 26; x++)
+  {
+    head->children[x] = NULL;
+  }
+  return head;
+}
+
+/* NOTE: int return values can be used to indicate errors 
+(typically non-zero) or success (typically zero return value) */
 
 /* TODO: change this return type */
 void indexPage(const char* url);
-
-int addWordOccurrence(const char* word, const int wordLength
-		       /* TODO: other parameters you need */);
-
+int addWordOccurrence(const char* word, const int wordLength/* TODO: other parameters you need */);
 void printTrieContents(/* TODO: any parameters you need */);
-
 int freeTrieMemory(/* TODO: any parameters you need */);
 
+//should not have to change.
 int getText(const char* srcAddr, char* buffer, const int bufSize);
 
+
+//starts
 int main(int argc, char** argv){
   /* TODO: write the (simple) main function
 
   /* argv[1] will be the URL to index, if argc > 1 */
+  node* head = newNode();
+
+
+
+  //Second to last step. Print the contents. 
+  printTrieContents(head);
+
+  //Last Step. Free memory.
+  freeTrieMemory(head);
 
   return 0;
 }
@@ -38,17 +64,35 @@ int main(int argc, char** argv){
 
 /* TODO: change this return type */
 void indexPage(const char* url)
-{}
+{
+  //todo
+}
 
-int addWordOccurrence(const char* word, const int wordLength
-		       /* TODO: other parameters you need */)
-{}
+int addWordOccurrence(const char* word, const int wordLength/* TODO: other parameters you need */)
+{
+  //todo
+}
 
-void printTrieContents(/* TODO: any parameters you need */)
-{}
+void printTrieContents(node* thisNode)
+{
+  
+}
 
-int freeTrieMemory(/* TODO: any parameters you need */)
-{}
+int freeTrieMemory(node* thisNode)
+{
+    if(thisNode != NULL)
+  {
+    for (int x = 0; x < 26; x++)
+    {
+      if(thisNode->children[x] != NULL)
+      {
+        freeTrieMemory(thisNode->children[x]);
+      }
+    }
+    free(thisNode);
+    thisNode = NULL;
+  }
+}
 
 /* You should not need to modify this function */
 int getText(const char* srcAddr, char* buffer, const int bufSize){
